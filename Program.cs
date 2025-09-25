0402a195d8ed3b88177855bb626d548be39dc24f
Doyth <yc8822473@gmail.com>
2025-09-24 20:52:03 -0500

    Proyecto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ejercicio 1: Positive Power");
+
+        Console.Write("Ingrese un número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine($"Resultado: {num * num}");
+        }
+        else if (num == 0)
+        {
+            Console.WriteLine("Resultado: 0");
+        }
+        else
+        {
+            Console.WriteLine("Resultado: Número negativo.");
+        }
+    }
+}
